1028317deadb1efdc412907b14b811ccf1ec15cc
Griffin Dooley <dooleygriffin@gmail.com>
2022-07-03 10:33:33 -0400

    commit tree f00ea4af13deb206be99582858e29cc324ef355d

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO 
+    bears (name, age, sex, color, temperament, alive) 
+VALUES
+    ('Mr. Chocolate', 197, 'F', 'turquoise', 'rowdy', 'true'),
+    ('Rowdy', 4, 'M', 'chartreuse', 'stinker', 'true'),
+    ('Tabitha', 18, 'F', 'maroon', 'party gal', 'true'),
+    ('Sergeant Brown', 10, 'F', 'every color except brown', 'goofball', 'true'),
+    ('Melissa', 67, 'M', 'heliotrope', 'just awful', 'true'),
+    ('Grinch', 12, 'M', 'black', 'gentle', 'true'),
+    ('Wendy', 11, 'M', 'cyan', 'under the weather', 'true'),
+    (NULL , 104, 'F', 'chrome', 'esoteric', 'true');
